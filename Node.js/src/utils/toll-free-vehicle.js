1822de36a31502646770450fa6483d01690e79a4
Magnus Ottosson <magnus@magnusottosson.se>
2019-10-09 20:18:44 +0200

    Implemented toll free vehicles

--- Node.js/src/utils/toll-free-vehicle.js
+++ Node.js/src/utils/toll-free-vehicle.js
@@ -1,10 +1,19 @@
+const TOLL_FREE_VEHICLE_TYPES = [
+  'diplomat',
+  'emergency',
+  'foreign',
+  'military',
+  'motorbike',
+  'tractor',
+]
+
 /**
  * Returns true if the vehicle type is toll free
  *
  * @param vehicleType
  */
 const isTollFreeVehicle = ({ vehicleType }) => {
-
+  return TOLL_FREE_VEHICLE_TYPES.includes(vehicleType)
 }
 
 module.exports = isTollFreeVehicle
